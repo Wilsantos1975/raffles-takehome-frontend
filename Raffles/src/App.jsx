af6034230dd4e7957b1ca1f7ba6ed0da7dd8bee3
Your Name <Faustosantos@pursuit.org>
2024-04-04 23:45:01 -0400

    rendered all raffles from back end

--- Raffles/src/App.jsx
+++ Raffles/src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
+const API_URL = import.meta.env.VITE_BASE_URL
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [raffles, setRaffles] = useState([])
+
+
+  const fetchData = async () => {
+    try {
+      // setLoading(true);
+      // setError("");
+      console.log(API_URL);
+      const response = await fetch(`${API_URL}/api/raffles`);
+      const {data}  = await response.json();
+      if (response.ok) {
+        console.log(data);
+
+        setRaffles(data);
+      } else {
+        throw new Error(errorMsg);
+      }
+    } catch (err) {
+      throw new Error(err.message);
+    } 
+    // finally {
+    //   setLoading(false);
+    // }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="App">  
+     <h1>Raffles</h1>
+      <ul>
+        {raffles.map((raffle) => (
+          <li key={raffle.id}>{raffle.name}</li>
+        ))} 
+        </ul>
+     </div>
     </>
   )
 }
